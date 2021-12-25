46d0ecda339d46055724098f2c8d5f7e83b5ec34
Jose Esteves <josefelixestevenpacheco@gmail.com>
2021-12-24 23:10:45 -0400

    mensafe de error acomodado. style acomodados. validacion echa

--- js/index.js
+++ js/index.js
@@ -1,5 +1,31 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", (e) => e.preventDefault());
-form.addEventListener("click", (e) => {
-
+const form = document.getElementById('form');
+const inputs = document.querySelectorAll('.form__input');
+const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+const validarForm = () => {
+  inputs.forEach((input) => {
+    if (input.value.trim() === '') {
+      input.parentElement.classList.add('form__contentInput--error');
+      input.classList.add('form__input--show');
+    } else {
+      input.parentElement.classList.remove(
+        'form__contentInput--error'
+      );
+      input.classList.remove('form__input--show');
+    }
+    if (emailRegex.test(form.EmailAddress.value.trim())) {
+      form.EmailAddress.parentElement.classList.remove(
+        'form__contentInput--error'
+      );
+      form.EmailAddress.classList.remove('form__input--show');
+    } else {
+      form.EmailAddress.parentElement.classList.add(
+        'form__contentInput--error'
+      );
+      form.EmailAddress.classList.add('form__input--show');
+    }
+  });
+};
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validarForm();
 });
